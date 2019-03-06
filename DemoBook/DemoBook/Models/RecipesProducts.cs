cb89c99981c1fb9422e912a0b155c1eb0731fcc5
Daniel10106 <dani.10103m@gmail.com>
2019-03-06 18:47:36 +0200

    Demo - Console Only

---
+++ DemoBook/DemoBook/Models/RecipesProducts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DemoBook.Models
+{
+    public partial class RecipesProducts
+    {
+        public int Id { get; set; }
+        public int? RecipeId { get; set; }
+        public int? ProductId { get; set; }
+
+        public virtual Product Product { get; set; }
+        public virtual Recipe Recipe { get; set; }
+    }
+}
