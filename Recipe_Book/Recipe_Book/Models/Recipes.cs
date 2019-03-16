13449669aab80cc80d7d173d149b01758ea5b51c
Daniel10106 <dani.10103m@gmail.com>
2019-03-16 11:54:08 +0200

    scaffolded

---
+++ Recipe_Book/Recipe_Book/Models/Recipes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Recipe_Book.Models
+{
+    public partial class Recipes
+    {
+        public Recipes()
+        {
+            ProductsRecipes = new HashSet<ProductsRecipes>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? Description { get; set; }
+        public int CategoryId { get; set; }
+        public string ImgName { get; set; }
+
+        public virtual Categories Category { get; set; }
+        public virtual ICollection<ProductsRecipes> ProductsRecipes { get; set; }
+    }
+}
