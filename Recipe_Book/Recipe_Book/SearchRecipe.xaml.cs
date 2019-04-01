2c5873c58a3f395f1dcc923ba8ecc17edbc2d7a7
daniil <danikillerxxxvolevo@gmail.com>
2019-04-01 14:21:38 +0300

    SearchRecipe

--- Recipe_Book/Recipe_Book/SearchRecipe.xaml.cs
+++ Recipe_Book/Recipe_Book/SearchRecipe.xaml.cs
@@ -20,11 +20,16 @@ namespace Recipe_Book
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class BlankPage1 : Page
+    public sealed partial class SearchRecipe : Page
     {
-        public BlankPage1()
+        public SearchRecipe()
         {
             this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
