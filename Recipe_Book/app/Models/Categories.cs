0fa2c0954cf487074d45f0db2e48c5ea3d5fe459
Мелани Ангелова <ani.angelova0910@abv.bg>
2019-04-01 13:32:30 +0300

    mel

---
+++ Recipe_Book/app/Models/Categories.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace app.Models
+{
+    public partial class Categories
+    {
+        public Categories()
+        {
+            Recipes = new HashSet<Recipes>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Recipes> Recipes { get; set; }
+    }
+}
