8dca0c0f5416522d2c05f1e865e8edf5fb187165
Даниел Бумбалов <dani.10103m@gmail.com>
2019-04-06 01:18:04 +0300

    Another very important commit

--- RecipeBook/Models/recipebookContext.cs
+++ RecipeBook/Models/recipebookContext.cs
@@ -27,7 +27,7 @@ namespace RecipeBook.Models
             {
                 //To change when pushing
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseMySQL("server=localhost;port=3306;user=root;password=CLusters10107#;database=recipebook");
+                optionsBuilder.UseMySQL("server=localhost;port=3306;user=root;password=root;database=recipebook");
             }
         }
 
